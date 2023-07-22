2cc065a10293300463f113a251e046f34ee4a8ce
JerezM <jerez149@gmail.com>
2023-07-22 19:06:02 -0300

    readme added and final script version added

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # telegram-difusion-tracking
+## Info:
+La idea del programa es generar una difusion a todos los miembros de un grupo de manera automatizada y trackear a su vez si ya se contactaron con dicho miembro del grupo o no.
+Para lograr esto se tuvieron que tener algunas consideraciones, por ejemplo para que telegram no detecte como spam o actividad sospechosa este programa, se tuvo que segmentar
+los msg que se mandan, asi que el programa lo que va a hacer es mandar mensajes de a tandas de 30 mensajes, y cada mensaje va a tener una pausa de 1min entre cada uno, como para
+simular un poco mas de realizmo y reducir las posibilidades de ban. 
+
+### Considerando esto, el programa va a tener 2 flujos de ejecucion:
+A) Cuando ejecutas el programa por primera vez que te va a preguntar los Grupos a los que queres mandar el mensaje, y te va a generar el archivo CSV con el nombre que vos elijas.
+ Una cuestion a considear de esto es que si el/los grupos que elijas tiene mas de 30 miembros en total, entonces la primera iteracion no va a llegar a mandarle el mensaje a todos
+ los miembros, por eso va a ser necesaria una segunda(o mas) iteraciones a futuro para poder mandar el msg deseado a todos los miembros.
+ 
+IMPORTARTE: No correr el programa mas de una vez por dia, porque si lo ejecutan 2 veces, entonces van a mandar muchos mensajes con la cuenta de rama y puede llevar a que los baneen.
+
+B) Cuando ya corriste el programa una vez y tenes un CSV creado pero que no se termino de mandar el mensaje a todos los miembros, podes elejir si queres continuar mandando los msg
+pendientes y el programa va a retomar desde donde se dejo la ultima vez. En caso de que el archivo CSV seleccionado ya este completado o vos elijas discontinuar la ejecucion, 
+entonces se va a reiniciar el programa y va a ejecutar lo mencionado en el paso A.
+
+## Modo de uso:
+1) Crear un archivo .txt situado en la carpeta donde este el ejecutable con el nombre "message-file.txt"
+2) Escribir el mensaje que queres enviar a los miembros adentro del .txt y guardarlo
+3) Ejecutar el programa
+
+4) Al iniciar te va a preguntar que pongas el nombre del CSV que queres checkear el estado(esto es para el caso en que haya un csv creado y toda tenga mensajes pendientes)
+4.1) Si el archivo existe y tiene mensajes pendientes entonces retoma la ejecucion y vuelve a mandar 30mensajes a los miembros que siguen
+4.2) En caso de que el archivo no exista o exista pero ya no tiene mensajes pendientes, sigue con el paso 5)
+
+5)Te va a preguntar los grupos que queres seleccionar para mandar el mensaje guardado en message-file.txt
+
+6)Una vez seleccionado los grupos, se va a empezar a mandar el mensaje a los distintos miembros, y luego de eso va a terminar el programa.
+
+## OBS:
+En el csv se va a guardar el nombre del grupo, la info para identificar al user y el status en relacion a los mensaje enviado(SENDED/NOT SENT/PENDING).
+En NOT SENT se aplica para los casos en que hubo algun error al mandar el mensaje y no se acepto el hacerlo, en caso de que pase se podria contartar manualmente porque capaz es un tema
+de telegram que flasho(ya si paso con muchos user me avisan porque seguro se depreco algo de la API de telegram o capaz hay algo que esta off y necesita un minifix)
--- send_messages.py
+++ send_messages.py
@@ -1,4 +1,5 @@
 import csv
+import time
 from telethon.sync import TelegramClient
 from telethon.errors import UserPrivacyRestrictedError
 from telethon.tl.functions.messages import SendMessageRequest
@@ -6,17 +7,27 @@ from telethon.tl.functions.messages import SendMessageRequest
 api_id = 20066700
 api_hash = 'af5de23471cd3ce06be10478e567c762'
 phone_number = '+542914733243'  # Your phone number with country code, e.g., +1234567890
+MESSAGE_SENT_THRESHOLD = 2 # Quantity of message that the script will sent
+MESSAGE_COOLDOWN_IN_SECONDS = 20 # Expressed in seconds 
+
+global_file_name = ""  # Global variable to store the file name
 
 # Dictionary of group IDs and their names
 groups = {
     -838510899: 'Testing script group',
     #'group2_id': 'Group 2 Name',
-    # Add more groups as needed
 }
+
+client = TelegramClient(phone_number, api_id, api_hash)
 
 # Function to update the CSV file
 def update_csv_file(group_name, member, message_sent):
-    with open('messages_tracker.csv', mode='a', newline='', encoding='utf-8') as file:
+    global global_file_name
+
+    if global_file_name == "":
+        global_file_name = input("Enter the name of the CSV file to store the tracking(include the '.csv'): ")
+
+    with open(global_file_name, mode='a', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
 
         # Check if the file is empty and write the header row if it is
@@ -25,36 +36,157 @@ def update_csv_file(group_name, member, message_sent):
 
         writer.writerow([group_name, member.username, member.first_name, member.last_name, member.id, message_sent])
 
-client = TelegramClient(phone_number, api_id, api_hash)
+def check_pending_status():
+    global global_file_name # Declare file_name as a global variable
+    pending_ids = []  # Lista para almacenar los ID con STATUS = "PENDING"
+
+    # Prompt the user to enter the name of the CSV file
+    global_file_name = input("Enter the name of the CSV file to check the pending status(include the '.csv'): ")
+
+    try:
+        with open(global_file_name, mode='r', newline='', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                if row['STATUS'] == 'PENDING':
+                    pending_ids.append(int(row['ID']))
+
+        if not pending_ids: 
+            print("The CSV selected has finished with the task. Restarting the program.")
+            global_file_name = ""
+    except FileNotFoundError:
+        print("The CSV selected doesn't exists. Restarting the program.")
+        global_file_name = ""
+        pass
+
+    return pending_ids
+
+def update_status_in_bulk(updated_statuses):
+    global global_file_name
+
+    try:
+        # Open the CSV file and read all the rows
+        with open(global_file_name, mode='r', newline='', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            rows = list(reader)
+
+        # Update the statuses in the rows based on the updated_statuses dictionary
+        for row in rows:
+            user_id = int(row['ID'])
+            if user_id in updated_statuses:
+                row['STATUS'] = updated_statuses[user_id]
+
+        # Write the updated rows back to the CSV file
+        with open(global_file_name, mode='w', newline='', encoding='utf-8') as file:
+            fieldnames = ['GROUP_NAME', 'USERNAME', 'FIRST_NAME', 'LAST_NAME', 'ID', 'STATUS']
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(rows)
+
+    except FileNotFoundError:
+        print(f"File {global_file_name} not found.")
+    except ValueError:
+        print("Invalid ID. Please provide a valid integer ID.")
+
+def parse_msg_from_file():
+    try:
+        with open("message-file.txt", 'r', encoding='utf-8') as file:
+            text = file.read()
+            return text
+    except FileNotFoundError:
+        print(f"File 'message-file.txt' not found.")
+        return ""
+
+async def process_initial_flow():
+    # Prompt for the message to send
+    msg = parse_msg_from_file()
+
+    # Display list of groups for selection
+    selected_groups = {}  # Variable to store selected groups
+    print("Select groups to send the message to:")
+    for group_id, group_name in groups.items():
+        selected = input(f"Group: {group_name} (Y/N): ").strip().lower()
+        selected_groups[group_id] = selected == 'y'
+
+    # Send the message to selected groups and update the CSV file
+    message_count = 0
+    for group_id, is_selected in selected_groups.items():
+        if is_selected:
+            group_name = groups[group_id]
+            groupEntity = await client.get_entity(group_id)
+            async for member in client.iter_participants(entity=groupEntity):
+                if message_count < MESSAGE_SENT_THRESHOLD:
+                    if not member.bot:
+                        try:
+                            await client(SendMessageRequest(peer=member, message=msg))
+                            print(f"Message sent to {member.username} - {member.first_name} {member.last_name}")
+                            update_csv_file(group_name, member, "SENDED")
+                            message_count += 1
+                        except UserPrivacyRestrictedError:
+                            print(f"Message could not be sent to {member.username} - {member.first_name} {member.last_name}. Privacy settings restricted.")
+                            update_csv_file(group_name, member, "NOT SENT")
+                            message_count += 1
+
+                        # Sleep for 1 minute before sending the next message
+                        print("1 min pause started...")
+                        time.sleep(MESSAGE_COOLDOWN_IN_SECONDS)
+                        print("1 min pause ended")
+
+                elif not member.bot:
+                    print(f"Message pending to {member.username} - {member.first_name} {member.last_name}")
+                    update_csv_file(group_name, member, "PENDING")
+
+async def process_in_progress_flow(pending_user_ids):
+    selected = input("You have a flow in progress, do you want to continue sending the messages? (Y/N): ").strip().lower() == 'y'
+
+    if selected:
+        msg = parse_msg_from_file()
+        message_count = 0
+
+        # Dictionary to store the updated statuses for each user ID
+        updated_statuses = {}
+
+        for user_id in pending_user_ids:
+            if message_count < MESSAGE_SENT_THRESHOLD:
+                member = await client.get_entity(user_id)
+                if (not member.bot):
+                        try:
+                            await client(SendMessageRequest(peer=member, message=msg))
+                            print(f"Message sent to {member.username} - {member.first_name} {member.last_name}")
+                            updated_statuses[user_id] = "SENDED"
+                            message_count += 1
+                        except UserPrivacyRestrictedError:
+                            print(f"Message could not be sent to {member.username} - {member.first_name} {member.last_name}. Privacy settings restricted.")
+                            updated_statuses[user_id] = "NOT SENT"
+                            message_count += 1
+
+                        print("1 min pause started...")
+                        time.sleep(MESSAGE_COOLDOWN_IN_SECONDS)
+                        print("1 min pause ended")
+
+            else:
+                print(f"Message pending to {user_id}")
+                updated_statuses[user_id] = "PENDING"
+        
+        # Update all the user IDs in the CSV file at once
+        update_status_in_bulk(updated_statuses)
+        return True
+
+    else:
+        return False
 
 async def main():
     try:
         await client.start()
 
-        # Prompt for the message to send
-        message = input("Enter the message you want to send:\n")
+        in_progress_finish = False
+        pending_user_ids = check_pending_status()
+        
+        if pending_user_ids:
+            in_progress_finish = await process_in_progress_flow(pending_user_ids)
 
-        # Display list of groups for selection
-        selected_groups = {}  # Variable to store selected groups
-        print("Select groups to send the message to:")
-        for group_id, group_name in groups.items():
-            selected = input(f"Group: {group_name} (Y/N): ").strip().lower()
-            selected_groups[group_id] = selected == 'y'
+        if not in_progress_finish:
+            await process_initial_flow()
 
-        # Send the message to selected groups and update the CSV file
-        for group_id, is_selected in selected_groups.items():
-            if is_selected:
-                group_name = groups[group_id]
-                groupEntity = await client.get_entity(group_id)
-                async for member in client.iter_participants(entity=groupEntity):
-                    if not member.bot:
-                        try:
-                            #await client(SendMessageRequest(peer=member, message=message))
-                            print(f"Message sent to {member.username} - {member.first_name} {member.last_name}")
-                            update_csv_file(group_name, member, "SENDED")
-                        except UserPrivacyRestrictedError:
-                            print(f"Message could not be sent to {member.username} - {member.first_name} {member.last_name}. Privacy settings restricted.")
-                            update_csv_file(member, "NOT SENDED")
     finally:
         await client.disconnect()
 
